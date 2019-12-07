75ad9899fd38cbc743a966fff4a42a7bc6ff3fa9
João Pedro Vasconcelos <47739139+joaopvasconcelos99@users.noreply.github.com>
2019-12-07 22:46:30 +0000

    Add files via upload

---
+++ App/views/types.js
@@ -0,0 +1,38 @@
+﻿define(['knockout'], function (ko) {
+    var vm = function () {
+        console.log('ViewModel initiated...')
+        //---Variáveis locais
+        var self = this;
+        var baseUri = 'http://192.168.160.58/pokemons/api/Pokemons/Types';
+        self.className = 'LaureadoIndividuos';
+        self.description = 'This page aims to demonstrate the use of the Nobel web API for laureates and the interconnection with other entities.<br>Called method(s): <ul><li>' + baseUri + '</li></ul>';
+        self.error = ko.observable();
+        self.types = ko.observableArray([]);
+        //--- Internal functions
+        function ajaxHelper(uri, method, data) {
+            self.error(''); // Clear error message
+            return $.ajax({
+                type: method,
+                url: uri,
+                dataType: 'json',
+                contentType: 'application/json',
+                data: data ? JSON.stringify(data) : null,
+                error: function (jqXHR, textStatus, errorThrown) {
+                    console.log("AJAX Call[" + uri + "] Fail...");
+                    self.error(errorThrown);
+                }
+            })
+        }
+        //--- Externel functions (accessible outside)
+        getTypes = function () {
+            console.log('CALL: Types...')
+            ajaxHelper(baseUri, 'GET').done(function (data) {
+                self.types(data);
+            });
+        };
+
+        //---- initial call
+        getTypes();
+    };
+    return vm;
+});
